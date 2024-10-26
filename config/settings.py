cf3cc5297971e1bd03e31202bcf178fd3093c48d
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2024-10-26 18:44:05 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g@4y#(1r$o__(4)&eu=7bsskgg$h=pzcypgvg@)kyw^faq@r$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
